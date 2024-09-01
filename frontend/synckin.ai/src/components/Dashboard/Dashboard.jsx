480fce8568afa251cd9188da7e5bb1e7347803c8
RithikRaja28 <rithikraja28.rr@gmail.com>
2024-09-01 12:23:39 +0530

    dashboard started

--- frontend/synckin.ai/src/components/Dashboard/Dashboard.jsx
+++ frontend/synckin.ai/src/components/Dashboard/Dashboard.jsx
@@ -1,24 +1,45 @@
-import React, { useContext, useEffect } from "react";
+// Dashboard.js
+import React, { useContext, useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Sidebar from "./utils/Sidebar";
 import Header from "./utils/Header";
-
 import "./Dashboard.css";
 import { AuthContext } from "../../context/AuthContext";
 import InventoryManager from "../InventoryManagement/InventorySystem";
 import TaskManager from "../TaskManagement/TaskManager";
 import IncomePage from "../Finance Tracker/IncomePage";
 import FinanceTracker from "../Finance Tracker/FinanceTracker";
+import IncomeChart from "../Finance Tracker/utils/IncomeChart";
+import axios from "axios";
+
 const Dashboard = () => {
   const navigate = useNavigate();
   const { auth } = useContext(AuthContext);
-  console.log(auth);
+  const [incomes, setIncomes] = useState([]);
+
   useEffect(() => {
     if (!auth.isAuthenticated) {
       navigate("/login");
       console.log("User is not authenticated. Redirecting to login...");
     }
+    
   }, [auth.isAuthenticated, navigate]);
+
+  useEffect(() => {
+    // Fetch income data for the chart
+    axios
+      .get("http://localhost:5000/api/income/show", {
+        headers: { "x-auth-token": localStorage.getItem("token") },
+      })
+      .then((response) => {
+        const incomeData = Array.isArray(response.data) ? response.data : [];
+        setIncomes(incomeData);
+      })
+      .catch((error) =>
+        console.error("There was an error fetching the incomes!", error)
+      );
+  }, []);
+
   return (
     <div className="d-flex vh-100 w-100">
       <Sidebar />
@@ -38,6 +59,7 @@ const Dashboard = () => {
                 <div>
                   <h1>Welcome to the Dashboard</h1>{" "}
                   {auth.user && <p>Hello, {auth.user.username}</p>}
+                  <IncomeChart incomes={incomes} />
                 </div>
               }
             />
